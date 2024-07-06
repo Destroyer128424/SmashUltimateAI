fd5b9cbf8c0ddd3dc7aab70e499e23b16900414a
Brady <bradycruse23@gmail.com>
2024-07-07 01:59:28 +0200

    Main update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2 as cv
 import numpy
 import time
-from vision import findClickPositions
+from vision import Vision
 import dxcam
 
 
 camera = dxcam.create()
+vision = Vision('Mario.jpg')
+camera.start(target_fps=60)  # threaded
 
 while True:
 
@@ -13,29 +15,25 @@ while True:
     last_time = time.time()
 
     # Get raw pixels from the screen, save it to a Numpy array
-    img = numpy.array(camera.grab())
+    img = numpy.array( camera.get_latest_frame())
     img = cv.cvtColor(img, cv.COLOR_BGRA2BGR)
     img = img[...,:3]
 
     # Display the picture
     #cv.imshow("OpenCV/Numpy normal", img)
-    findClickPositions("NEW.jpg", img, 0.4, 'rectangles')
+    points = vision.find(img, 0.7, 'rectangles')
 
     #Prints FPS
     print(f"fps: {1 / (time.time() - last_time)}")
 
 
     
-
-    
     '''CODE ADD HERE'''
-
-
-
 
 
 
     # Press "q" to quit
     if cv.waitKey(25) & 0xFF == ord("q"):
         cv.destroyAllWindows()
+        camera.stop()
         break
--- vision.py
+++ vision.py
@@ -3,84 +3,81 @@ import numpy as np
 import os
 
 
+class Vision:
 
+    needle_img = None
+    needle_h = 0
+    needle_w = 0
+    method = None
 
-def findClickPositions(needle_img_path, haystack_img, threshold=0.5, debug_mode=None):
+    def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
 
-    needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+        self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+            #Save the dimensions of the needle image
+        self.needle_w = self.needle_img.shape[1]
+        self.needle_h = self.needle_img.shape[0]
+        self.method = method
 
-    #Save the dimensions of the needle image
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
+    def find(self, haystack_img, threshold=0.5, debug_mode=None):
 
-    method = cv.TM_CCOEFF_NORMED
-    result = cv.matchTemplate(haystack_img, needle_img, method)
+        result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
 
-    #Get the positions from the match results that exceed ourn threshold
-    locations = np.where(result >= threshold)
-    locations = list(zip(*locations[::-1]))
-    #print(locations)
+        #Get the positions from the match results that exceed ourn threshold
+        locations = np.where(result >= threshold)
+        locations = list(zip(*locations[::-1]))
+        #print(locations)
 
-    # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
-    # locations by using groupRectangles().
-    # First we need to create the list of [x, y, w, h] rectangles
-    rectangles = []
-    for loc in locations:
-        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
-        # Add every box to the list twice in order to retain single (non-overlapping) boxes
-        rectangles.append(rect)
-        rectangles.append(rect)
-    # Apply group rectangles.
-    # The groupThreshold parameter should usually be 1. If you put it at 0 then no grouping is
-    # done. If you put it at 2 then an object needs at least 3 overlapping rectangles to appear
-    # in the result. I've set eps to 0.5, which is:
-    # "Relative difference between sides of the rectangles to merge them into a group."
-    rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
-    #print(rectangles)
+        # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
+        # locations by using groupRectangles().
+        # First we need to create the list of [x, y, w, h] rectangles
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
+            # Add every box to the list twice in order to retain single (non-overlapping) boxes
+            rectangles.append(rect)
+            rectangles.append(rect)
+        # Apply group rectangles.
+        # The groupThreshold parameter should usually be 1. If you put it at 0 then no grouping is
+        # done. If you put it at 2 then an object needs at least 3 overlapping rectangles to appear
+        # in the result. I've set eps to 0.5, which is:
+        # "Relative difference between sides of the rectangles to merge them into a group."
+        rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+        #print(rectangles)
 
-    points = []
-    if len(rectangles):
-        #print('Found needle.')
+        points = []
+        if len(rectangles):
+            #print('Found needle.')
 
-        line_color = (0, 255, 0)
-        line_type = cv.LINE_4
-        marker_color = (255, 0, 255)
-        marker_type = cv.MARKER_CROSS
+            line_color = (0, 255, 0)
+            line_type = cv.LINE_4
+            marker_color = (255, 0, 255)
+            marker_type = cv.MARKER_CROSS
 
-        # Loop over all the rectangles
-        for (x, y, w, h) in rectangles:
+            # Loop over all the rectangles
+            for (x, y, w, h) in rectangles:
 
-            # Determine the center position
-            center_x = x + int(w/2)
-            center_y = y + int(h/2)
-            # Save the points
-            points.append((center_x, center_y))
+                # Determine the center position
+                center_x = x + int(w/2)
+                center_y = y + int(h/2)
+                # Save the points
+                points.append((center_x, center_y))
 
-            if debug_mode == 'rectangles':
-                # Determine the box position
-                top_left = (x, y)
-                bottom_right = (x + w, y + h)
-                # Draw the box
-                cv.rectangle(haystack_img, top_left, bottom_right, color=line_color,
-                            lineType=line_type, thickness=2)
-            elif debug_mode == 'points':
-                # Draw the center point
-                cv.drawMarker(haystack_img, (center_x, center_y),
-                            color=marker_color, markerType=marker_type,
-                            markerSize=40, thickness=2)
+                if debug_mode == 'rectangles':
+                    # Determine the box position
+                    top_left = (x, y)
+                    bottom_right = (x + w, y + h)
+                    # Draw the box
+                    cv.rectangle(haystack_img, top_left, bottom_right, color=line_color,
+                                lineType=line_type, thickness=2)
+                elif debug_mode == 'points':
+                    # Draw the center point
+                    cv.drawMarker(haystack_img, (center_x, center_y),
+                                color=marker_color, markerType=marker_type,
+                                markerSize=40, thickness=2)
 
-    if debug_mode:
-        cv.imshow('Matches', haystack_img)
-        #cv.waitKey()
-        #cv.imwrite('result_click_point.jpg', haystack_img)
+        if debug_mode:
+            cv.imshow('Matches', haystack_img)
+            #cv.waitKey()
+            #cv.imwrite('result_click_point.jpg', haystack_img)
 
-    return points
-
-
-#points = findClickPositions('albion_cabbage.jpg', 'albion_farm.jpg', debug_mode='points')
-#print(points)
-#points = findClickPositions('albion_turnip.jpg', 'albion_farm.jpg', 
-#                            threshold=0.70, debug_mode='rectangles')
-#print(points)
-#print('Done.')
-
+        return points
